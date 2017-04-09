fef185d9c2b1cc19020db07b6d8e9d3751abe0b3
hellosylvee <helloimsylvia@gmail.com>
2017-04-08 22:34:35 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   #save new recipe
   post '/recipes' do
     @recipe = Recipe.create(name: params['name'], ingredients: params['ingredients'], cook_time: params['cook_time'])
-    redirect to '/recipes/#{@recipe.id}'
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   #view a recipe
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.update(name: params['name'], ingredients: params['ingredients'], cook_time: params['cook_time'])
     @recipe.save
-    redirect to "/posts/#{recipe.id}"
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id/delete' do
